2c2641c64af15f892787a3ffceb3cbaf78ea5650
erik-meier <errmeier@gmail.com>
2024-05-21 08:00:39 -0500

    refactor monster loading

--- src/encounter-builder/encounter-builder.py
+++ src/encounter-builder/encounter-builder.py
@@ -2,18 +2,21 @@ import pandas as pd
 from pathlib import Path
 from pprint import pprint
 
+def filter_monsters_by_cr(df):
+    """
+    Monsters with a challenge rating of N/A are companions or retainers
+    and should not be used for encounter building
+    """
+    return df[df["Challenge Rating"] != "N/A"]
+
 def load_monsters():
     """
     Reads all CSV files from resources directory to compile monsters list.
     """
     dir = Path("resources")
     files = dir.glob("*.csv")
-    data = []
-    for file in files:
-        df = pd.read_csv(file)
-        df = df[df["Challenge Rating"] != "N/A"]
-        data.append(df)
-    return pd.concat(data)
+    df = pd.concat(pd.read_csv(file) for file in files)
+    return filter_monsters_by_cr(df)
 
 if __name__=="__main__":
     data = load_monsters()
